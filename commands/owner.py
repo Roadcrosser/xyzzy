02d78c6427d56713d6e24f4da1e713adce3edb9a
Ovyerus <Ovyerus@users.noreply.github.com>
2017-09-07 20:55:57 +1000

    Some more owner commands

--- commands/owner.py
+++ commands/owner.py
@@ -1,5 +1,7 @@
 from command_sys import command
 import inspect
+import asyncio
+import re
 
 class Owner:
     def __init__(self, xyzzy):
@@ -23,6 +25,58 @@ class Owner:
             out = await out
 
         await ctx.send("```py\n{}\n```".format(out))
+
+    @command(owner=True)
+    async def shutdown(self, ctx):
+        """
+        After confirmation, shuts down the bot and all running games.
+        [This command may only be used by trusted individuals.]
+        """
+        if self.xyzzy.channels:
+            await ctx.send("```diff\n"
+                           "!There are currently {} games running on my system.\n"
+                           "-If you shut me down now, all unsaved data regarding these games could be lost!\n"
+                           "(Use `{}nowplaying` for a list of currently running games.)".format(len(self.xyzzy.channels), self.xyzzy.invoker * 2))
+
+        await ctx.send("```md\n"
+                       "## Are you sure you want to shut down the bot? ##\n"
+                       "[y/n]:\n"
+                       "```")
+
+        while True:
+            try:
+                check = lambda x: x.channel == ctx.msg.channel and x.author == ctx.msg.author
+                msg = await self.xyzzy.wait_for("message", check=check, timeout=30)
+
+                if re.match(r"^`?({})?y(es)?`?$".format(self.xyzzy.invoker * 2), msg.content.lower()):
+                    await ctx.send("```asciidoc\n"
+                                   ".Xyzzy.\n"
+                                   "// Now shutting down...\n"
+                                   "```")
+                    await self.xyzzy.logout()
+                elif re.match(r"^`?({})?no?`?$".format(self.xyzzy.invoker * 2), msg.content.lower()):
+                    return await ctx.send("```css\n"
+                                          "Shutdown aborted.\n"
+                                          "```")
+                else:
+                    await ctx.send("```md\n"
+                                   "# Invalid response. #\n"
+                                   "```")
+            except asyncio.TimeoutError:
+                return await ctx.send("```css\n"
+                                      "Message timeout: Shutdown aborted.\n"
+                                      "```")
+
+    @command()
+    async def announce(self, ctx):
+        if not ctx.args:
+            return await ctx.send("Nothing to announce.")
+
+        for _, chan in self.xyzzy.channels:
+            try:
+                await chan.channel.send("``````".format(ctx.clean))
+            except:
+                pass
 
 def setup(xyzzy):
     return Owner(xyzzy)
