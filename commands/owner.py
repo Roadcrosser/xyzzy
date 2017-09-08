27390debd16ea1628a53e8b8accff4bfb240b70e
Ovyerus <Ovyerus@users.noreply.github.com>
2017-09-08 19:51:55 +1000

    Formatting tweaks and add module reload command

--- commands/owner.py
+++ commands/owner.py
@@ -38,10 +38,7 @@ class Owner:
                            "-If you shut me down now, all unsaved data regarding these games could be lost!\n"
                            "(Use `{}nowplaying` for a list of currently running games.)".format(len(self.xyzzy.channels), self.xyzzy.invoker * 2))
 
-        await ctx.send("```md\n"
-                       "## Are you sure you want to shut down the bot? ##\n"
-                       "[y/n]:\n"
-                       "```")
+        await ctx.send("```md\n## Are you sure you want to shut down the bot? ##\n[y/n]:\n```")
 
         while True:
             try:
@@ -49,34 +46,42 @@ class Owner:
                 msg = await self.xyzzy.wait_for("message", check=check, timeout=30)
 
                 if re.match(r"^`?({})?y(es)?`?$".format(self.xyzzy.invoker * 2), msg.content.lower()):
-                    await ctx.send("```asciidoc\n"
-                                   ".Xyzzy.\n"
-                                   "// Now shutting down...\n"
-                                   "```")
+                    await ctx.send("```asciidoc\n.Xyzzy.\n// Now shutting down...\n```")
                     await self.xyzzy.logout()
                 elif re.match(r"^`?({})?no?`?$".format(self.xyzzy.invoker * 2), msg.content.lower()):
-                    return await ctx.send("```css\n"
-                                          "Shutdown aborted.\n"
-                                          "```")
+                    return await ctx.send("```css\nShutdown aborted.\n```")
                 else:
-                    await ctx.send("```md\n"
-                                   "# Invalid response. #\n"
-                                   "```")
+                    await ctx.send("```md\n# Invalid response. #\n```")
             except asyncio.TimeoutError:
-                return await ctx.send("```css\n"
-                                      "Message timeout: Shutdown aborted.\n"
-                                      "```")
+                return await ctx.send("```css\nMessage timeout: Shutdown aborted.\n```")
 
-    @command()
+    @command(owner=True, usage="[ announcement ]")
     async def announce(self, ctx):
+        """
+        For each channel currently playing a game, sends the text in [announcement].
+        [This command may only be used by trusted individuals.]
+        """
         if not ctx.args:
-            return await ctx.send("Nothing to announce.")
+            return await ctx.send("```diff\n-Nothing to announce.\n```")
 
         for _, chan in self.xyzzy.channels:
             try:
-                await chan.channel.send("``````".format(ctx.clean))
+                await chan.channel.send("```{}```".format(ctx.clean))
             except:
                 pass
+
+    @command(usage="[ module ]", owner=True, has_site_help=False)
+    async def reload(self, ctx):
+        """
+        Reloads the module specified in [module].
+        [This command may only be used by trusted individuals.]
+        """
+
+        if not ctx.args:
+            return await ctx.send("```diff\n-No module to reload.\n```")
+
+        self.xyzzy.commands.reload_module('commands.' + ctx.args[0].lower())
+        await ctx.send('```diff\n+Reloaded module "{}".\n```'.format(ctx.args[0].lower()))
 
 def setup(xyzzy):
     return Owner(xyzzy)
