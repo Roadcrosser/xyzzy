f754c9daff4f7b9e927478428f2cde54baea2c9d
Ovyerus <Ovyerus@users.noreply.github.com>
2017-09-11 20:53:48 +1000

    Let quetzal_parser read bytes object

--- quetzal_parser.py
+++ quetzal_parser.py
@@ -27,47 +27,50 @@ def read_word(address: int, mem: List[int]) -> int:
     """Read's a 16-bit value at the specified address."""
     return (mem[address] << 8) + mem[address + 1]
 
-# TODO: Let this read file-like objects/bytes.
-def parse_quetzal(path: str) -> HeaderData:
+def parse_quetzal(fp) -> HeaderData:
     """Reads a Quetzal save file, and returns information about the associated game."""
-    if type(path) != str:
-        raise TypeError("path is not a string.")
+    if type(fp) != str and not hasattr(fp, 'read'):
+        raise TypeError("file is not a string or a bytes-like object.")
 
-    if not os.path.isfile(path):
-        raise Exception("File provided isn't a file, or doesn't exist.")
+    if type(fp) == str:
+        if not os.path.isfile(fp):
+            raise Exception("File provided isn't a file, or doesn't exist.")
 
-    # Open file as bytes
-    with open(path, 'rb') as qzl:
-        header = qzl.read(4)
+        # Open file as bytes
+        qzl = open(fp, 'rb')
+    else:
+        qzl = fp
 
-        # Header checking yay.
-        if header != b"FORM":
-            raise Exception("Invalid file format.")
+    header = qzl.read(4)
 
-        qzl.read(4) # Skip some bytes we don't care about.
+    # Header checking yay.
+    if header != b"FORM":
+        raise Exception("Invalid file format.")
 
-        ftype = qzl.read(4)
+    qzl.read(4) # Skip some bytes we don't care about.
 
-        # More header checking yay.
-        if ftype != b"IFZS":
-            raise Exception("Invalid file format.")
+    ftype = qzl.read(4)
 
-        chunk = Chunk(qzl)
-        name = chunk.getname()
-        size = chunk.getsize()
-        data = chunk.read(size).decode("ansi")
+    # More header checking yay.
+    if ftype != b"IFZS":
+        raise Exception("Invalid file format.")
 
-        # Make sure first chunk is IFhd.
-        if name != b"IFhd":
-            raise Exception("File does not start with an IFhd chunk.")
-        elif size != 13:
-            raise Exception("Invalid size for IFhd chunk: " + str(size))
+    chunk = Chunk(qzl)
+    name = chunk.getname()
+    size = chunk.getsize()
+    data = chunk.read(size).decode("ansi")
 
-        # Bitwise magic to get data.
-        release = (ord(data[0]) << 8) + ord(data[1])
-        serial = int(data[2:8])
-        checksum = (ord(data[8]) << 8) + ord(data[9])
-        # pc = (ord(data[10]) << 16) + (ord(data[11]) << 8) + ord(data[12]) # This isn't needed rn, but it's commented just in case.
+    # Make sure first chunk is IFhd.
+    if name != b"IFhd":
+        raise Exception("File does not start with an IFhd chunk.")
+    elif size != 13:
+        raise Exception("Invalid size for IFhd chunk: " + str(size))
+
+    # Bitwise magic to get data.
+    release = (ord(data[0]) << 8) + ord(data[1])
+    serial = int(data[2:8])
+    checksum = (ord(data[8]) << 8) + ord(data[9])
+    # pc = (ord(data[10]) << 16) + (ord(data[11]) << 8) + ord(data[12]) # This isn't needed rn, but it's commented just in case.
 
     return HeaderData(release, serial, checksum)
 
