094c7d676ba3aa74232b490178d4a0ef0fa6d5eb
Ovyerus <Ovyerus@users.noreply.github.com>
2017-10-05 20:27:12 +1100

    Add message splitting for messages that get too stronk

--- modules/command_sys.py
+++ modules/command_sys.py
@@ -44,7 +44,29 @@ class Context:
             # Escape bad mentions
             content = str(content).replace("@everyone", "@\u200Beveryone").replace("@here", "@\u200Bhere")
 
-        msg = await self._send(content, dest, embed=embed, file=file, files=files)
+        msg = None
+
+        if content and len(content) > 2000:
+            if content.find('```') == -1 or content.find('```', content.find('```') + 3) == -1:
+                await self._send(content[:2000], dest, embed=embed, file=file, files=files)
+                await self.send(content[2000:], dest=dest)
+            elif content.find('```', content.find('```') + 3) + 2 < 2000:
+                await self._send(content[:content.find('```', content.find('```') + 3) + 3], dest,
+                                 embed=embed, file=file, files=files)
+                await self.send(content[content.find('```', content.find('```') + 3) + 3:], dest=dest)
+            else:
+                start_block = content[content.find('```'):content.find('\n', content.find('```')) + 1]
+
+                if content.find('\n', content.find('```')) == content.rfind('\n', 0, 2000):
+                    split_cont = content[:1996] + '\n```'
+                    content = start_block + content[1996:]
+                else:
+                    split_cont = content[:content.rfind('\n', 0, content.rfind('\n', 0, 2000) + 1)][:1996] + '\n```'
+                    content = start_block + content[len(split_cont) - 4:]
+
+                await self.send(split_cont + content, dest=dest, embed=embed, file=file, files=files)
+        else:
+            msg = await self._send(content, dest, embed=embed, file=file, files=files)
 
         return msg
 
