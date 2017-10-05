79d389025e7fdf71d53eb53e6555f30e27aadc9e
Roadcrosser <roadcrosser0@gmail.com>
2017-10-05 22:02:38 +0800

    Abstract functions and add filepath checks

--- modules/game_channel.py
+++ modules/game_channel.py
@@ -101,13 +101,40 @@ class GameChannel:
 
         self.process.stdin.write((input + "\n").encode("latin-1", "replace"))
 
+    async def parse_output(self, buffer):
+        if buffer != b"":
+            out = buffer.decode("latin-1", "replace")
+            msg = ""
+
+            for i, line in enumerate(out.splitlines()):
+                if line.strip() == ".":
+                    line = ""
+
+                line = line.replace("*", "\*").replace("_", "\_").replace("~", "\~")
+
+                if len(msg + line[self.indent:] + "\n") < 2000:
+                    msg += line[self.indent:] + "\n"
+                else:
+                    await self.send_game_output(msg)
+
+                    msg = line[self.indent:]
+
+            msg = msg.strip()
+            saves = self.check_saves()
+
+            if self.first_time:
+                saves = None
+                self.first_time = False
+
+            await self.send_game_output(msg, saves)
+
     async def game_loop(self):
         """Enters into the channel's game process loop."""
         if not self.process:
             await self.init_process()
 
         buffer = b""
-        first_time = True
+        self.first_time = True
         self.playing = True
 
         while self.process.returncode is None:
@@ -115,32 +142,10 @@ class GameChannel:
                 output = await asyncio.wait_for(self.process.stdout.read(1), 0.5)
                 buffer += output
             except asyncio.TimeoutError:
-                if buffer != b"":
-                    out = buffer.decode("latin-1", "replace")
-                    msg = ""
+                await self.parse_output(buffer)
+                buffer = b""
 
-                    for i, line in enumerate(out.splitlines()):
-                        if line.strip() == ".":
-                            line = ""
-
-                        line = line.replace("*", "\*").replace("_", "\_").replace("~", "\~")
-
-                        if len(msg + line[self.indent:] + "\n") < 2000:
-                            msg += line[self.indent:] + "\n"
-                        else:
-                            await self.send_game_output(msg)
-
-                            msg = line[self.indent:]
-
-                    msg = msg.strip()
-                    saves = self.check_saves()
-
-                    if first_time:
-                        saves = None
-                        first_time = False
-
-                    await self.send_game_output(msg, saves)
-
+                if os.path.exists(self.save_path):
                     files = os.listdir(self.save_path)
                     latest = 0
 
@@ -151,8 +156,6 @@ class GameChannel:
                             os.unlink("{}/{}".format(self.save_path, file))
                         elif mod_time > latest and not SCRIPT_OR_RECORD.match(file):
                             latest = mod_time
-
-                    buffer = b""
 
         self.playing = False
 
@@ -245,19 +248,19 @@ class GameChannel:
 
     def check_saves(self):
         """Checks if the user saved the game."""
-        files = [x for x in os.listdir(self.save_path) if not SCRIPT_OR_RECORD.match(x) and x != "__UPLOADED__.qzl"]
-        latest = [0, None]
+        if os.path.exists(self.save_path):
+            files = [x for x in os.listdir(self.save_path) if not SCRIPT_OR_RECORD.match(x) and x != "__UPLOADED__.qzl"]
+            latest = [0, None]
 
-        for file in files:
-            mod_time = os.stat("{}/{}".format(self.save_path, file)).st_mtime_ns
+            for file in files:
+                mod_time = os.stat("{}/{}".format(self.save_path, file)).st_mtime_ns
 
-            if mod_time > latest[0]:
-                latest = [mod_time, file]
+                if mod_time > latest[0]:
+                    latest = [mod_time, file]
 
-        if latest[1] and latest[1] != self.last_save:
-            self.last_save = latest[1]
-            return discord.File("{}/{}".format(self.save_path, latest[1]), latest[1])
-        else:
+            if latest[1] and latest[1] != self.last_save:
+                self.last_save = latest[1]
+                return discord.File("{}/{}".format(self.save_path, latest[1]), latest[1])
             return None
 
     def cleanup(self):
